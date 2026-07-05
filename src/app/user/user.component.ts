7408c50d4d9c0fabe5db36c4b0a8ef375c74f4d1
Faeze Rahmati <rahmati.faeze1997@gmail.com>
2026-07-05 15:12:40 +0330

    refactor(app): pass user object to app-user component
    - Replace individual input properties with a single 'user' object
    - Update user selection logic to use the new 'user' object
    - Simplify HTML markup in both parent and child components

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,6 +8,17 @@ import {
   Output,
 } from '@angular/core';
 
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// };
+
+interface User {
+  id: string;
+  name: string;
+  avatar: string;
+}
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -16,12 +27,7 @@ import {
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
-  // id = input.required<string>();
-  // avatar = input.required<string>();
-  // name = input.required<string>();
+  @Input({ required: true }) user!: User;
 
   @Output() select = new EventEmitter<string>();
   // select = output<string>();
@@ -29,9 +35,9 @@ export class UserComponent {
   // imagePath = computed(() => `../../assets/users/${this.avatar()}`);
 
   get imagePath() {
-    return `../../assets/users/${this.avatar}`;
+    return `../../assets/users/${this.user.avatar}`;
   }
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
