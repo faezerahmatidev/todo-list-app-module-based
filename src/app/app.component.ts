a96e9e3138761facb508e459e1148172df87aa25
Faeze Rahmati <rahmati.faeze1997@gmail.com>
2026-07-08 14:15:26 +0330

    Convert project to NgModule structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './user/user.component';
+import { Component } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
-import { TasksComponent } from './tasks/tasks.component';
-import { NgFor, NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
